cd5caa0ec37696226cb3783bd17a83a291bba588
Rizqy <estrizqy@gmail.com>
2024-06-10 09:26:15 +0700

    fix: dropdown profile signout btn

--- resources/js/Components/DropdownMenu.vue
+++ resources/js/Components/DropdownMenu.vue
@@ -51,7 +51,6 @@ const user = computed(() => page.props.auth.user);
                     <Link
                         :href="route('logout')"
                         method="post"
-                        as="button"
                         class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
                         role="menuitem"
                         >Sign out</Link

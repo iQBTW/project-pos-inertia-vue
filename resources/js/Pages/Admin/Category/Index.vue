f2e40f60ef84de23c9a13b56ee54b5768008e1b8
Rizqy <estrizqy@gmail.com>
2024-12-01 06:23:12 +0700

    style: menambahkan icon edit dan delete pada action table

--- resources/js/Pages/Admin/Category/Index.vue
+++ resources/js/Pages/Admin/Category/Index.vue
@@ -296,30 +296,33 @@ const deleteCategory = (id) => {
             <div class="w-full px-2 bg-gray-300/50 min-h-screen pt-2">
                 <fwb-table hoverable>
                     <fwb-table-head>
-                        <fwb-table-head-cell>No</fwb-table-head-cell>
-                        <fwb-table-head-cell>Name</fwb-table-head-cell>
-                        <fwb-table-head-cell>Action</fwb-table-head-cell>
+                        <fwb-table-head-cell class="text-center">No</fwb-table-head-cell>
+                        <fwb-table-head-cell class="text-center">Name</fwb-table-head-cell>
+                        <fwb-table-head-cell class="text-center">Action</fwb-table-head-cell>
                     </fwb-table-head>
                     <fwb-table-body>
                         <fwb-table-row
                             v-for="(category, index) in categories.data"
                             :key="category.id"
                         >
-                            <fwb-table-cell>{{ index + 1 }}</fwb-table-cell>
-                            <fwb-table-cell>{{ category.name }}</fwb-table-cell>
-                            <fwb-table-cell class="flex gap-2">
+                            <fwb-table-cell class="text-center">{{ index + 1 }}</fwb-table-cell>
+                            <fwb-table-cell class="text-center">{{ category.name }}</fwb-table-cell>
+                            <fwb-table-cell class="flex items-center justify-center">
                                 <Button
-                                    class="transition-all ease-in 3s border border-yellow-300 hover:text-white hover:bg-yellow-300"
+                                    class="transition-all ease-in 3s hover:text-yellow-300"
                                     @click="openEditModal(category, index)"
                                 >
-                                    Edit
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                    </svg>
                                 </Button>
                                 <Button
-                                    severity="danger"
-                                    class="transition-all ease-in 3s border hover:text-white hover:bg-red-800"
+                                    class="transition-all ease-in 3s hover:text-red-800"
                                     @click.prevent="openDeleteModal(category, index)"
                                 >
-                                    Delete
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                    </svg>
                                 </Button>
                             </fwb-table-cell>
                         </fwb-table-row>

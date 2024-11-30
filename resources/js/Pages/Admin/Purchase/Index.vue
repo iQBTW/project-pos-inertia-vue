672c55755643beac87450a0b65e81d32b463bc3b
Rizqy <estrizqy@gmail.com>
2024-12-01 06:47:00 +0700

    style: merubah layouting

--- resources/js/Pages/Admin/Purchase/Index.vue
+++ resources/js/Pages/Admin/Purchase/Index.vue
@@ -164,117 +164,131 @@ const storeOrder = () => {
     <Head title="Dashboard Shop" />
 
     <AdminLayout>
-        <div class="flex justify-center pb-2 pt-5">
-            <div class="px-2 w-full rounded-md shadow-md pb-10">
+        <div class="flex justify-center">
+            <div class="px-4 w-full rounded-md shadow-md">
                 <form @submit.prevent="storeOrder">
-                    <div class="px-5 pb-2">
-                        <div class="px-0 2xl:px-5 pb-2">
-                            <div
-                                v-for="product in products"
-                                :key="product.id"
-                                class="flex items-center gap-2 w-fit py-2"
-                            >
-                                <img
-                                    :src="product.image ?
-                                        `/storage/${product.product_images[0]?.image}`
-                                            : NoImage
-                                    "
-                                    alt="Product Image"
-                                    class="flex w-24 h-24"
-                                />
-                                <h2>{{ product.name }}</h2>
-                                <p>
-                                    Price: {{ formatCurrency(product.price) }}
-                                </p>
-                                <input
-                                    class="w-[100px] rounded-md border-0 ring-1 ring-slate-700 focus:border-0 focus:ring-primary-500 transition-all ease-in-out 3s"
-                                    type="number"
-                                    v-model="product.qty"
-                                    min="0"
-                                    placeholder="Quantity"
-                                />
-                                <div class="py-4">
-                                    <span
-                                        class="text-lg bg-blue-500 text-white px-2 py-1 rounded-full hover:bg-blue-600 cursor-pointer"
-                                        @click="addToCart(product)"
-                                    >
-                                        +
-                                    </span>
+                    <div class="flex mt-4 items-baseline justify-evenly gap-3 w-full bg-gray-300/70 rounded-md">
+                        <div class=" px-4 mt-4  w-[500px] 2xl:w-1/2">
+                            <div class="px-0 2xl:px-5 pb-2">
+                                <div
+                                    v-for="product in products"
+                                    :key="product.id"
+                                    class="flex items-center gap-2 w-fit py-2"
+                                >
+                                    <img
+                                        :src="product.image ?
+                                            `/storage/${product.product_images[0]?.image}`
+                                                : NoImage
+                                        "
+                                        alt="Product Image"
+                                        class="flex w-24 h-24"
+                                    />
+                                    <h2>{{ product.name }}</h2>
+                                    <p>
+                                        Price: {{ formatCurrency(product.price) }}
+                                    </p>
+                                    <input
+                                        class="w-[100px] rounded-md border-0 ring-1 ring-slate-700 focus:border-0 focus:ring-primary-500 transition-all ease-in-out 3s"
+                                        type="number"
+                                        v-model="product.qty"
+                                        min="0"
+                                        placeholder="Quantity"
+                                    />
+                                    <div class="py-4">
+                                        <span
+                                            class="text-lg bg-blue-500 text-white px-2 py-1 rounded-full hover:bg-blue-600 cursor-pointer"
+                                            @click="addToCart(product)"
+                                        >
+                                            +
+                                        </span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="my-5">
-                        <h3 class="text-lg font-semibold">Cart Summary</h3>
-                        <table
-                            class="w-full border-collapse border border-slate-300"
-                        >
-                            <thead>
-                                <tr class="bg-gray-100">
-                                    <th
-                                        class="border border-slate-300 py-2 px-4"
-                                    >
-                                        Item
-                                    </th>
-                                    <th
-                                        class="border border-slate-300 py-2 px-4"
-                                    >
-                                        Qty
-                                    </th>
-                                    <th
-                                        class="border border-slate-300 py-2 px-4"
-                                    >
-                                        Unit Price
-                                    </th>
-                                    <th
-                                        class="border border-slate-300 py-2 px-4"
-                                    >
-                                        Actions
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr v-for="item in cart" :key="item.id">
-                                    <td
-                                        class="border border-slate-300 py-2 px-4"
-                                    >
-                                        {{ item.name }}
-                                    </td>
-                                    <td
-                                        class="border border-slate-300 py-2 px-4 text-center"
-                                    >
-                                        {{ item.qty }}
-                                    </td>
-                                    <td
-                                        class="border border-slate-300 py-2 px-4 text-right"
-                                    >
-                                        {{
-                                            formatCurrency(item.unit_price) || 0
-                                        }}
-                                    </td>
-                                    <td
-                                        class="border border-slate-300 py-2 px-4 text-center"
-                                    >
-                                        <button
-                                            class="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
-                                            @click="
-                                                removeFromCart(item.product_id)
-                                            "
-                                        >
-                                            Remove
-                                        </button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <div class="text-left mt-3">
-                            <strong
-                                >Total:
-                                {{ formatCurrency(cartTotal) || 0 }}</strong
+                        <div class="">
+                            <h3 class="text-lg font-semibold pb-2">Cart Summary</h3>
+                            <table
+                                class="w-full border-collapse border border-primary-500"
                             >
+                                <thead>
+                                    <tr class="bg-gray-100">
+                                        <th
+                                            class="text-center border border-primary-500 py-2 px-5"
+                                        >
+                                            No
+                                        </th>
+                                        <th
+                                            class="text-center border border-primary-500 py-2 px-5"
+                                        >
+                                            Item
+                                        </th>
+                                        <th
+                                            class="text-center border border-primary-500 py-2 px-5"
+                                        >
+                                            Qty
+                                        </th>
+                                        <th
+                                            class="text-center border border-primary-500 py-2 px-5"
+                                        >
+                                            Unit Price
+                                        </th>
+                                        <th
+                                            class="text-center border border-primary-500 py-2 px-5"
+                                        >
+                                            Actions
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="item, index in cart" :key="item.id">
+                                        <td
+                                            class="text-center border border-primary-500 py-2 px-5"
+                                        >
+                                            {{ index+1 }}
+                                        </td>
+                                        <td
+                                            class="text-center border border-primary-500 py-2 px-5"
+                                        >
+                                            {{ item.name }}
+                                        </td>
+                                        <td
+                                            class="text-center border border-primary-500 py-2 px-5"
+                                        >
+                                            {{ item.qty }}
+                                        </td>
+                                        <td
+                                            class="text-center border border-primary-500 py-2 px-5"
+                                        >
+                                            {{
+                                                formatCurrency(item.unit_price) || 0
+                                            }}
+                                        </td>
+                                        <td
+                                            class="text-center border border-slate-300 py-2 px-5 "
+                                        >
+                                            <button
+                                                class="transition ease-in-out duration-300 hover:text-red-600"
+                                                @click="
+                                                    removeFromCart(item.product_id)
+                                                "
+                                            >
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                            </svg>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <div class="text-left mt-3">
+                                <strong
+                                    >Total:
+                                    {{ formatCurrency(cartTotal) || 0 }}</strong
+                                >
+                            </div>
                         </div>
                     </div>
-                    <div class="flex items-baseline gap-3 w-full">
+                    <div class="flex items-baseline justify-end gap-3 w-full pt-4">
                         <div class="flex gap-2 pb-2 2xl:px-5">
                             <div class="flex flex-col gap-2 w-[295px]">
                                 <label for="price">Amount</label>

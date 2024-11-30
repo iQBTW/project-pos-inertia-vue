2f678079a087092cb55404d8241488352dc31e51
Rizqy <estrizqy@gmail.com>
2024-12-01 06:22:45 +0700

    delete: menghapus menu product category

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -252,43 +252,6 @@ const showSidebar = ref(true);
                                 <template #default>Category</template>
                             </fwb-sidebar-item>
                         </Link>
-                        <Link :href="route('product_category.index')">
-                            <fwb-sidebar-item
-                                class="hover:bg-primary-highlight transition-all ease-in 3s mt-2"
-                                :class="{
-                                    '!bg-primary-active-color !text-white':
-                                        route().current('product_category.index'),
-                                }"
-                            >
-                                <template #icon>
-                                    <svg
-                                        class="flex-shrink-0 w-6 h-6 text-gray-500 transition-all ease-in duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white active:text-gray-900 dark:active:text-white"
-                                        :class="{
-                                            '!text-white':
-                                                route().current(
-                                                    'product_category.index'
-                                                ),
-                                        }"
-                                        aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="24"
-                                        height="24"
-                                        fill="currentColor"
-                                        viewBox="0 0 24 24"
-                                    >
-                                        <path
-                                            fill-rule="evenodd"
-                                            d="M20 10H4v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8ZM9 13v-1h6v1a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1Z"
-                                            clip-rule="evenodd"
-                                        />
-                                        <path
-                                            d="M2 6a2 2 0 0 1 2-2h16a2 2 0 1 1 0 4H4a2 2 0 0 1-2-2Z"
-                                        />
-                                    </svg>
-                                </template>
-                                <template #default>Product Category</template>
-                            </fwb-sidebar-item>
-                        </Link>
                         <Link :href="route('order.index')">
                             <fwb-sidebar-item
                                 class="hover:bg-primary-highlight transition-all ease-in 3s mt-2"
